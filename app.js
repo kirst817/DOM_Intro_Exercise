f4348f9cde08e12c47943b46b9b6dbd502d4ec22
Kirsten <kirstenwedde@gmail.com>
2016-04-16 19:27:33 -0600

    Started DOM exercise

--- app.js
+++ app.js
@@ -1,1 +1,46 @@
 console.log("Javascript is alive!");
+
+window.onload()= function {
+
+
+var greeting = document.getElementById('greeting');
+greeting.innerText = "Hello, World!";
+
+document.getElementsByTagName('li').style.backgroundColor = 'yellow';
+
+var image = document.createElement('img');
+image.src = "http://49.media.tumblr.com/tumblr_m6qt1rjPSz1rxjzkho1_500.gif";
+div.appendChild('img');
+
+
+// Add the class of selected to an <li> when it is clicked. Remove it from any other lis as well.
+document.querySelectorAll('li')
+addEventListener('click', function(){
+  this.addClass('selected');
+});
+document.querySelectorAll('li')
+addEventListener('click', function(){
+  this.removeClass('selected');
+});
+// Change the image to be the most recently clicked food item.
+document.querySelector(image).addEventListener('click', function(){
+  this.
+});
+// When the gray div is moused over, it's removed from the DOM.
+document.querySelector("#ghosting").addEventListener("mouseover", function() {
+  this.remove();
+});
+// When the orange div is moused over, its width doubles. When the mouse moves out of the div, it returns to its original size.
+document.querySelector("resize").addEventListener("mouseover", function(){
+  this.style.width = "400px";
+});
+document.querySelector("resize").addEventListener("mouseover", function(){
+  this.style.width = "200px";
+});
+// When the reset button is clicked - remove the selected class from each <li> and change the image to panic.jpeg.
+document.querySelector('input')
+// When the 1, 2, 3, 4, 5, 6, 7, 8, 9, or 0 key is pressed, the page alerts the message "I HATE NUMBERZZZ!"
+document.
+// // BONUS: If someone types the Konami Code, the page alerts "YOU ARE AN EVENT HANDLER GURUUUUUUUUU!"
+
+});
