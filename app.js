7e9d6c15009edb3af23caf8c0ae4f512060302c6
Kirsten <kirstenwedde@gmail.com>
2016-04-24 13:50:17 -0600

    Finished DOM

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ for (var i = 0; i < lis.length; i++) {
 }
 
 var image = document.createElement('img');
+ $(image).addClass('picture');
+
 image.src = "http://49.media.tumblr.com/tumblr_m6qt1rjPSz1rxjzkho1_500.gif";
 var greetDiv = document.getElementById('greeting')
 greetDiv.appendChild(image);
@@ -20,18 +22,26 @@ greetDiv.appendChild(image);
 // // Add the class of selected to an <li> when it is clicked. Remove it from any other lis as well.
 var li = document.querySelectorAll('li');
 for (var i = 0; i < li.length; i++) {
-    var lis = li[i].className = "";
+//loop through and add event listener
  li[i].addEventListener('click', function(e){
+   for (var i = 0; i < li.length; i++) {
+     //loop through and remove className
+
+    var lis = li[i].className = "";
+  }
     var target = e.target;
     var selected = $(target).addClass('selected');
-
-   console.log(target);
-
-   greetDiv.setAttribute("src", "/images/" + target.innerText + ".jpeg")
-     //populate picture in image when food item is clicked
-
-  });
+  })
 }
+
+
+     for (var i = 0; i < li.length; i++) {
+       var picture = document.querySelector('img');
+       li[i].addEventListener('click', function(e){
+        picture.setAttribute("src", "./images/" + e.target.innerHTML + ".jpeg");
+      //  populate picture in image when food item is clicked
+  })
+  };
 
 var ghosting = document.getElementById('ghosting');
 ghosting.addEventListener('mouseover', function(){
@@ -51,11 +61,11 @@ document.getElementById('resize').addEventListener("mouseout", function(){
 var lis = document.getElementsByTagName('li');
 document.getElementById('reset').addEventListener("click", function(){
         $(lis).removeClass("selected")
-        // image.src =`panic.jpeg`
+picture.setAttribute("src", "./images/" + 'panic.jpeg');
     });
 
 window.addEventListener("keypress", function(e){
-if(typeOf(e.keyCode) === "number" )
+if(e.keyCode <= 57 && e.keyCode >= 48)
 alert("I HATE NUMBERZZZ!");
 
   });
